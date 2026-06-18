bb95245ebf4494a795db8fb7a8fa34c09b43df9b
Guha_09 <souravguha2025@gmail.com>
2026-06-18 19:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.08%) - LeetSync

--- daily_question/1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ daily_question/1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double in=0.5*(double)minutes;
-        double mini=(double)minutes/5;
-        double ans=0.00;
-        double a1,a2,a3;
-    //    if(minutes<hour) minutes
-        if(hour==12) hour=0;
-       if(mini>hour)
-        a1=abs(hour-mini)*30-in;
+        double in = 0.5 * (double)minutes;
+        double mini = (double)minutes / 5;
+        double ans = 0.00;
+        double a1, a2, a3;
+
+        if (hour == 12)
+            hour = 0;
+        if (mini > hour)
+            a1 = abs(hour - mini) * 30 - in;
         else
-        a1=abs(hour-mini)*30+in;
-        
-        hour=hour+12;
-        a2=abs(hour-mini)*30+in;
-        mini=mini+12;
-        hour=hour-12;
-        a3=abs(hour-mini)*30-in;
-        ans=min(a1,min(a2,a3));
-        return ans>0?ans:abs(ans);
+            a1 = abs(hour - mini) * 30 + in;
+
+        hour = hour + 12;
+        a2 = abs(hour - mini) * 30 + in;
+        mini = mini + 12;
+        hour = hour - 12;
+        a3 = abs(hour - mini) * 30 - in;
+        ans = min(a1, min(a2, a3));
+        return ans > 0 ? ans : abs(ans);
     }
 };
