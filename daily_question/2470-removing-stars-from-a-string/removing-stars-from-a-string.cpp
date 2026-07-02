7a97f0caa05af7d9abccf3cadf0e1fc90e6d6be1
Guha_09 <souravguha2025@gmail.com>
2026-07-02 19:17:32 +0530

    Time: 1725 ms (5.17%) | Memory: 26.2 MB (96.33%) - LeetSync

---
+++ daily_question/2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='*'){
+                s.erase(i-1,2);
+                i-=2;
+             }
+        }
+        return s;
+    }
+};
