dd7a3b8148e261a9105455c474b3d239c3b60bb9
Guha_09 <souravguha2025@gmail.com>
2026-07-24 14:46:40 +0530

    Time: 558 ms (73.03%) | Memory: 35.4 MB (80.34%) - LeetSync

---
+++ daily_question/3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int mx = *max_element(nums.begin(), nums.end());
+
+        int lim = 1;
+        while (lim <= mx) lim <<= 1;
+        lim <<= 1;               
+
+        vector<bool> s1(lim, false);
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s1[nums[i] ^ nums[j]] = true;
+            }
+        }
+
+        vector<bool> s2(lim, false);
+
+        for (int x = 0; x < lim; x++) {
+            if (!s1[x]) continue;
+            for (int v : nums) {
+                s2[x ^ v] = true;
+            }
+        }
+
+        int cnt = 0;
+        for (bool x : s2)
+            cnt += x;
+
+        return cnt;
+    }
+};
