fa70841b0ccf9aebf94f65fa3184e85612bb4a07
Guha_09 <souravguha2025@gmail.com>
2026-08-31 22:39:02 +0530

    Time: 4 ms (99.46%) | Memory: 94.7 MB (95.21%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -0,0 +1,22 @@
+class Solution {
+ public:
+  int longestCycle(vector<int>& edges) {
+    int ans = -1; 
+    int time = 1; 
+    vector<int> timeVisited(edges.size()); 
+    for (int i = 0; i < edges.size(); ++i) {
+      if (timeVisited[i])
+        continue;
+     int startTime = time; 
+      int u = i; 
+      while (u != -1 && !timeVisited[u]) {
+        timeVisited[u] = time++; 
+        u = edges[u]; 
+      }
+      if (u != -1 && timeVisited[u] >= startTime)
+        ans = max(ans, time - timeVisited[u]);
+    }
+
+    return ans; 
+  }
+};
